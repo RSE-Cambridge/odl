fe887e61be6584a6a679b14d650e5639c646e478
Piero <piero.coronica@gmail.com>
2021-02-18 10:17:16 +0000

    FIX All WFBP implementations mask rays not hitting the detector (output not constant along z) - TODO: full requires too much memory

--- odl/tomo/operators/wfbp.py
+++ odl/tomo/operators/wfbp.py
@@ -10,88 +10,122 @@ from odl.tomo.geometry import Geometry
 
 def wfbp(recon_space, geometry, proj_data):
     """
-    First implementation of WFBP - WIP does not consider z!
+    First implementation of WFBP - At most 1 ray per angle!
     """
     angles = geometry.angles
+    R = geometry.det_radius + geometry.src_radius
 
-    V = np.empty(recon_space.shape + angles.shape, dtype='float32', order='C')
+    V = np.zeros(recon_space.shape + angles.shape, dtype='float32', order='C')
 
     for ix,iy,iz in np.ndindex(recon_space.shape):
         x,y,z = recon_space.grid[ix,iy,iz]
 
         for itheta in range(angles.shape[0]):
             theta = angles[itheta]
-            # clockwise rotation of theta 
+            # get u, v - clockwise rotation by theta 
             u = x*np.cos(theta) + y*np.sin(theta)
+            l = -x*np.sin(theta) + y*np.cos(theta) 
+            z_src = theta * geometry.pitch / (2 * np.pi)
+            v = (z - z_src) * R / (l + geometry.src_radius)
+
             iu = geometry.det_partition.byaxis[0].index(u)
-            V[ix,iy,iz, itheta] = np.sum(proj_data[itheta,iu,:])
+            try:
+                iv = geometry.det_partition.byaxis[1].index(v)
+                V[ix,iy,iz, itheta] = proj_data[itheta,iu,iv]
+            except:
+                pass
             
     return np.sum(V, axis=-1)
 
 def wfbp_theta(recon_space, geometry, proj_data):
     """
-    Implementation of WFBP with vectorised computation on theta - WIP does not consider z!
+    Implementation of WFBP with vectorised computation on theta - TODO: multi book!
     """
     angles = geometry.angles
-    upart = geometry.det_partition.byaxis[0]
-    u_min = upart.min_pt
-    u_cell = upart.cell_sides
+    zs_src = angles * geometry.pitch / (2 * np.pi)
+    R = geometry.det_radius + geometry.src_radius
+
+    u_min, v_min = geometry.det_partition.min_pt
+    u_max, v_max = geometry.det_partition.max_pt
+    u_cell, v_cell = geometry.det_partition.cell_sides
+    
 
     V_tot = np.empty(recon_space.shape, dtype='float32', order='C')
 
     for ix,iy,iz in np.ndindex(recon_space.shape):
         x,y,z = recon_space.grid[ix,iy,iz]
-        
-        us =  x*np.cos(angles) + y*np.sin(angles)
+        # get u,v for rays throug the voxel at each angle
+        # clockwise rotation by angles
+        us = x*np.cos(angles) + y*np.sin(angles)
+        ls = -x*np.sin(angles) + y*np.cos(angles)
+        vs = (z - zs_src) * R / (ls + geometry.src_radius)
+
+        # add mask for rays hitting the detector
+        mask  = vs > v_min
+        mask *= vs < v_max
+
+        us = us[mask]
+        vs = vs[mask]
+
         ius = (us - u_min) // u_cell
         ius = ius.astype(np.int)
-        V_tot[ix,iy,iz] = np.sum(proj_data[np.arange(angles.shape[0]),ius,:])
-    return V_tot
+        ivs = (vs - v_min) // v_cell
+        ivs = ivs.astype(np.int)
 
-def wfbp_theta(recon_space, geometry, proj_data):
-    """
-    Implementation of WFBP with vectorised computation on angles - WIP does not consider z!
-    """
-    angles = geometry.angles
-    upart = geometry.det_partition.byaxis[0]
-    u_min = upart.min_pt
-    u_cell = upart.cell_sides
+        ithetas = np.arange(angles.shape[0])[mask]
 
-    V_tot = np.empty(recon_space.shape, dtype='float32', order='C')
-
-    for ix,iy,iz in np.ndindex(recon_space.shape):
-        x,y,z = recon_space.grid[ix,iy,iz]
-        
-        us =  x*np.cos(angles) + y*np.sin(angles)
-        ius = (us - u_min) // u_cell
-        ius = ius.astype(np.int)
-        V_tot[ix,iy,iz] = np.sum(proj_data[np.arange(angles.shape[0]),ius,:])
+        V_tot[ix,iy,iz] = np.sum(proj_data[ithetas,ius,ivs])
     return V_tot
 
 def wfbp_full(recon_space, geometry, proj_data):
     """
-    Implementation of WFBP fully vectorised - WIP does not consider z!
+    Implementation of WFBP fully vectorised - TODO: requires a lot of memory
     This is just an exercise!
     """
     angles = geometry.angles
-    upart = geometry.det_partition.byaxis[0]
-    u_min = upart.min_pt
-    u_cell = upart.cell_sides
+    zs_src = angles * geometry.pitch / (2 * np.pi)
+    zs_src = zs_src.reshape((1,1,1,-1))
+    R = geometry.det_radius + geometry.src_radius
+
+    u_min, v_min = geometry.det_partition.min_pt
+    u_max, v_max = geometry.det_partition.max_pt
+    u_cell, v_cell = geometry.det_partition.cell_sides
 
     V_tot = np.empty(recon_space.shape, dtype='float32', order='C')
 
     x, y, z = recon_space.grid.coord_vectors
     # shapes: (x, 1, angles) + (1, y, angles) -> (x,y,angles)
     # given x,y,theta compute u = x*np.cos(theta) + y*np.sin(theta)
-    us = (np.outer(x, np.cos(angles)).reshape((x.shape[0],1,angles.shape[0])) +
-          np.outer(y, np.sin(angles)).reshape((1,y.shape[0],angles.shape[0])))
-    # get grid indices of us
-    ius = (us - u_min) // u_cell
-    ius = ius.astype(np.int)
-    # ithetas same shape as ius, for each (x,y,theta) provides the index of theta
-    ithetas = np.tile(np.arange(angles.shape[0]), (x.shape[0],y.shape[0],1))
-    single_slice = np.sum(proj_data[ithetas,ius,:], axis=(-2,-1))
-    # TODO: z is not considered!!! 
-    V_tot = np.tile(np.expand_dims(single_slice,-1), (1, V_tot.shape[2]))
+    us = (np.outer(x, np.cos(angles)).reshape((x.shape[0],1,1,angles.shape[0])) +
+          np.outer(y, np.sin(angles)).reshape((1,y.shape[0],1,angles.shape[0])))
+    ls = (np.outer(x, -np.sin(angles)).reshape((x.shape[0],1,1,angles.shape[0])) +
+          np.outer(y, np.cos(angles)).reshape((1,y.shape[0],1,angles.shape[0])))
+    vs = (z.reshape((1,1,-1,1)) - zs_src) * R / (ls + geometry.src_radius)
+    
+    mask  = vs < v_min
+    mask += vs > v_max
+
+    # us = us[mask]
+    # vs = vs[mask]
+    # get grid indices
+    ius = ((us - u_min) // u_cell).astype(np.int16)
+    ivs = ((vs - v_min) // v_cell).astype(np.int16)
+
+    # Deal with ivs out of range
+    _proj_data = np.pad(proj_data, [(0,0),(0,0),(0,1)], mode='constant')
+    ivs[mask] = -1
+    print("_proj_data.shape",_proj_data.shape)
+    # # ithetas same shape as ius, for each (x,y,theta) provides the index of theta
+    ithetas = np.tile(np.arange(angles.shape[0]), (x.shape[0],y.shape[0],z.shape[0],1)).astype(np.int)
+    ius = np.tile(ius, (1,1,z.shape[0],1)).astype(np.int)
+    print("ithetas.shape",ithetas.shape)
+    print("ius.shape",ius.shape)
+    print("ivs.shape",ivs.shape)
+
+    V = _proj_data[ithetas,ius,ivs]
+    print(V.shape)
+    V_tot = np.sum(V, axis=-1)
+    # # TODO: z is not considered!!! 
+    # V_tot = np.tile(np.expand_dims(single_slice,-1), (1, V_tot.shape[2]))
 
     return V_tot
