8366563a5fc47aa28c07b2cdbde1306bc57a82b8
Piero <piero.coronica@gmail.com>
2021-03-10 15:14:58 +0000

    Tensor type as argument; allocate memory for x,y,z on GPU

--- odl/tomo/operators/wfbp_torch.py
+++ odl/tomo/operators/wfbp_torch.py
@@ -9,69 +9,21 @@ import torch
 from odl.discr import DiscretizedSpace
 from odl.tomo.geometry import Geometry
 
-def wfbp_theta_torch(recon_space, geometry, proj_data):
-    """
-    First Torch implementation - TODO: remove it
-    """
-    cuda = torch.device('cuda')
-    
-    
-    proj_data = torch.tensor(proj_data, device=cuda)
-    angles = torch.tensor(geometry.angles, device=cuda)
-    zs_src = angles * geometry.pitch / (2 * np.pi)
-    R = geometry.det_radius + geometry.src_radius
-
-    u_min, v_min = geometry.det_partition.min_pt
-    u_max, v_max = geometry.det_partition.max_pt
-    u_cell, v_cell = geometry.det_partition.cell_sides
-
-
-    V_tot = torch.empty(recon_space.shape, dtype=torch.float32, device=cuda)
-
-    # Other indices
-    us = torch.empty(angles.shape, dtype=torch.float32, device=cuda)
-    vs = torch.empty(angles.shape, dtype=torch.float32, device=cuda)
-    ls = torch.empty(angles.shape, dtype=torch.float32, device=cuda)
-
-    for ix,iy,iz in np.ndindex(recon_space.shape):
-        x,y,z = recon_space.grid[ix,iy,iz]
-        # get u,v for rays throug the voxel at each angle
-        # clockwise rotation by angles
-        us = x*torch.cos(angles) + y*torch.sin(angles)
-        ls = -x*torch.sin(angles) + y*torch.cos(angles)
-        vs = (z - zs_src) * R / (ls + geometry.src_radius)
-
-        # add mask for rays hitting the detector
-        mask  = vs > v_min
-        mask *= vs < v_max
-
-        us = us[mask]
-        vs = vs[mask]
-
-        ius = ((us - u_min) // u_cell).to(torch.int64)
-        ivs = ((vs - v_min) // v_cell).to(torch.int64)
-
-        ithetas = torch.arange(angles.shape[0], dtype=torch.long, device=cuda)[mask]
-
-        V_tot[ix,iy,iz] = torch.sum(proj_data[ithetas,ius,ivs])
-    return V_tot.cpu()
-
-def wfbp_full_torch(recon_space, geometry, proj_data):
+def wfbp_full_torch(recon_space, geometry, proj_data, dtype=torch.float32):
     """
     Torch implementation of WFBP - TODO: improve performances
     TODO: COPYING THE OUTPUT BACK TO CPU IS TOO SLOW
 
-    Tested on det: (736,64) angles: 500 volume: (512,512,96) ~ 4.1 s (0.48s + copy)
+    Tested on det: (736,64) angles: 500 volume: (512,512,96) ~ 3.7 s (3.4 s float16)
 
     Note: uncomment code to change V memory layout (x, y, z, angles) -> (angles, x, y, z)
-    Tested on det: (736,64) angles: 500 volume: (512,512,96) ~ 3.8 s (0.90s + copy)
-    copying data to CPU is faster.. why?
+    Tested on det: (736,64) angles: 500 volume: (512,512,96) ~ 3.5 s (3.2 s float16)
     """
 
     cuda = torch.device('cuda')
     _proj_data = np.pad(proj_data, [(0,0),(0,0),(0,1)], mode='constant')    
-    _proj_data = torch.tensor(_proj_data, device=cuda)
-    angles = torch.tensor(geometry.angles, device=cuda)
+    _proj_data = torch.tensor(_proj_data, dtype=dtype, device=cuda)
+    angles = torch.tensor(geometry.angles, dtype=dtype, device=cuda)
     
     zs_src = angles * geometry.pitch / (2 * np.pi)
     zs_src = zs_src.reshape((1,1,-1))
@@ -82,13 +34,16 @@ def wfbp_full_torch(recon_space, geometry, proj_data):
     u_max, v_max = geometry.det_partition.max_pt
     u_cell, v_cell = geometry.det_partition.cell_sides
 
-    V_tot = torch.empty(recon_space.shape, dtype=torch.float32, device=cuda)
-
     x, y, z = recon_space.grid.coord_vectors
-    x = torch.tensor(x, device=cuda)
-    y = torch.tensor(y, device=cuda)
-    z = torch.tensor(z, device=cuda)   
-        
+    x = torch.tensor(x, dtype=dtype, device=cuda)
+    y = torch.tensor(y, dtype=dtype, device=cuda)
+    z = torch.tensor(z, dtype=dtype, device=cuda)
+
+
+    V_tot = torch.empty(recon_space.shape, dtype=dtype, device=cuda)
+    V = torch.empty((x.shape[0],y.shape[0],angles.shape[0]), dtype=dtype, device=cuda)
+    # V = torch.empty((angles.shape[0],x.shape[0],y.shape[0]), dtype=dtype, device=cuda)
+
     ithetas = np.tile(np.arange(angles.shape[0]), (x.shape[0],y.shape[0],1))
 #     shape = (angles.shape[0],x.shape[0],y.shape[0])
 #     ithetas = np.repeat(np.arange(shape[0]), np.prod(shape[1:])).reshape(shape)
@@ -109,7 +64,7 @@ def wfbp_full_torch(recon_space, geometry, proj_data):
     
     # get grid indices
     ius = ((us - u_min) // u_cell).to(torch.int64)
-    del us
+    del us, x, y
 
     vs = (z[0] - zs_src) * R / (ls + geometry.src_radius)
     _ivs = ((vs - v_min) / v_cell)
